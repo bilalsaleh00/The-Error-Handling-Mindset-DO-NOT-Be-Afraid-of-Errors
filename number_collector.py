6c2a877c258bf92c19d9e38509b47d91ae23d924
bilalsaleh <bilalsaleh00@yahoo.com>
2026-08-09 15:49:29 -0700

    Add number collector

---
+++ number_collector.py
@@ -0,0 +1,28 @@
+try:
+    number1 = int(input("Enter number 1:"))
+except ValueError:
+    print("That's not a valid number. Using 0 instead.")
+    number1 = 0
+
+try:
+    number2 = int(input("Enter number 2:"))
+except ValueError:
+    print("That's not a valid number. Using 0 instead.")
+    number2 = 0
+
+
+try:
+    number3 = int(input("Enter number 3:"))
+except ValueError:
+    print("That's not a valid number. Using 0 instead.")
+    number3 = 0
+
+total = number1 + number2 + number3
+average = total / 3
+print(f"your numbers:{number1}, {number2}, {number3}")
+
+print(f"sum:{total}")
+print(f"Average: {average:.2f}")
+
+
+
